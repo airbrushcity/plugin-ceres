46f7ee8bfacec59030cb87f68a10d759b522d6b4
airbrushcity <a.kolbfleisch@airbrush-city.de>
2022-08-25 09:58:57 +0200

    upload-1

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    require("./tools/webpack/scripts.config"),
-    require("./tools/webpack/scripts-client.config"),
-    require("./tools/webpack/scripts-server.config"),
-    require("./tools/webpack/styles.config"),
-    require("./tools/webpack/scripts-custom.config")
-];
